09a669483ee1c00563d0a19bb1bade42316fc9d5
computerkunst <71874970+computerkunst@users.noreply.github.com>
2023-03-10 13:21:17 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'HAWK Physical Computing'
+copyright = '2023, JLK'
+author = 'JLK'
 
-release = '0.1'
-version = '0.1.0'
+release = '0.0'
+version = '0.0.0'
 
 # -- General configuration
 
