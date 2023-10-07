694a36dc06276c1a6a56c3067e76a932fe613e4e
computerkunst <medien.digital@mail.de>
2023-10-07 22:42:05 +0200

    fix jquery

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
-    "sphinxcontrib.jquery",
+    'sphinxcontrib.jquery',
 ]
 
 intersphinx_mapping = {
@@ -30,7 +30,7 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
-html_theme = 'sphinx_rtd_theme'
+#html_theme = 'sphinx_rtd_theme'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
